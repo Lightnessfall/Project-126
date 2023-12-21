820cb16b43c69ea2f35becea20724cb361c612f1
Lightnessfall <114197164+Lightnessfall@users.noreply.github.com>
2023-12-20 19:52:43 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+var song = "";
+
+function preload(){
+    song = loadSound("music.mp3");
+}
+function setup(){
+    canvas = createCanvas(500, 500);
+    canvas.position(750, 300);
+    video = createCapture(VIDEO);
+    video.hide();
+}
+function draw(){
+    image(video, 0, 0, 500, 500);
+}
+function play(){
+    song.play();
+}
